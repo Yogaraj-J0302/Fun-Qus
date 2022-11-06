ab3ac36a5c89e4f7639403cb2f9eae1bc52c0a90
Yogaraj J <105450205+Yogaraj-J0302@users.noreply.github.com>
2022-11-06 12:36:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+function createEle(ele,att,value){
+    let x=document.createElement(ele);
+    x.setAttribute(att,value);
+    return x;
+}
+let container=createEle("div","class","container");
+let h1=createEle("h1","class","content");
+h1.innerHTML="Are You Made..?"
+let br1=document.createElement("br");
+let yes=createEle("input","type","radio");
+yes.setAttribute("id","yes")
+yes.setAttribute("value","yes")
+yes.setAttribute("name","selection");
+let yesLable=createEle("lable","for","yes");
+yesLable.innerHTML="Yes"
+let no=createEle("input","type","radio");
+no.setAttribute("id","no")
+no.setAttribute("value","no")
+no.setAttribute("name","selection");
+let noLable=createEle("lable","for","no");
+noLable.innerHTML="No"
+let button=createEle("button","type","button");
+button.innerHTML="Click Here";
+button.addEventListener("click",click);
+no.addEventListener("change",()=>{
+    if(no.checked) button.style.display="none";
+})
+yes.addEventListener("change",()=>{
+    if(yes.checked) button.style.display=null;
+})
+function click(){
+    let x=document.getElementsByName("selection");
+    let xValue;
+    let button_css=document.querySelector("button")
+    for(let i=0; i<x.length; i++){
+        if(x[i].checked){
+            xValue=x[i].value;
+            if(xValue=="no"){
+                button_css.style.display=null;
+            } else{
+                NoFun();
+            }           
+        } 
+    }
+}
+function NoFun(){
+    let x=document.querySelector("h1");
+    x.setAttribute("id","ans");
+    x.innerHTML="Correct-u..! Correct-u..! Correct-u..!";
+    return x;
+}
+container.append(h1,br1,yes,yesLable,no,noLable,br1,button);
+document.body.append(container);
+
